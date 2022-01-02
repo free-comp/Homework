f4e646612928da83a3daf2e9f7ee638df302262d
Alisa_Brk <free-comp@yandex.ru>
2022-01-03 00:50:49 +0300

    Добавлено решение задачи с использованием методов

--- Program.cs
+++ Program.cs
@@ -1,52 +1,70 @@
 ﻿// Программа формирует из массива случайных чисел массив из четных чисел и выводит в консоль.
 
 // Алгоритм:
-// 1. Инициализируем переменную - размер массива, сам массив, ставим счетчик на 0 элемент
-// 2. Заполняем массив случайными целыми числами от -10 до 10, выводим на экран
+// 1. Инициализируем переменную - размер массива, а также сам массив.
+// 2. Заполняем массив случайными целыми числами от -10 до 10.
+// 3. Выводим на в консоль.
 // 3. Инициализируем новый пустой массив такого же размера, что и первоначальный
 // 4. Ищем четные числа в первоначальном массиве, для чего проходимся по всем его элементам
-// 5. Найденные четные числа записываем в новый пустой массив
-// 6. Проверяем, если в новый массив остался пустым, то выводим надпись, что в первонач.
-//    массиве не было четных чисел
-// 7. Иначе выводим содержимое нового массива.
+// 5. Найденные четные числа записываем в выходной массив
+// 6. Проверяем, если не было найдено четных чисел, то выводим надпись, сообщающую об этом.
+// 7. Иначе выводим в консоль содержимое нового массива.
 
 
-int SizeArray = 10; // чтобы можно было менять размер только в одном месте
-
-int[] Array = new int[SizeArray];
-int index = 0;
-Console.WriteLine("Дан массив случайных целых чисел: ");
-while (index < SizeArray)
+// метод заполнения массива
+void FillArray(int[] array)
 {
-    Array[index] = new Random().Next(-10, 11); // заполняем
-    Console.Write($"{Array[index]} "); // выводим в консоль
-    index++;
-}
-Console.WriteLine();
-
-int[] ResultArray = new int[SizeArray];
-int position = 0;
-
-for (int i = 0; i < SizeArray; i++)
-{
-    if (Array[i] % 2 == 0)
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
     {
-        ResultArray[position] = Array[i]; // в нов.массив кладем только четные значения
-        position++;
+        array[i] = new Random().Next(-10, 11);
     }
 }
 
-if (position == 0) // если позиция осталась на нул.месте, то в нов.массив ничего не положили
+// метод вывода элементов массива в консоль
+void PrintArray(int[] array, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+// inputArray - исходный массив
+// outputArray - конечный массив
+// evenCount - счетчик четных элементов
+int SearchEven(int[] inputArray, int[] outputArray)
+{
+    int length = inputArray.Length;
+    int evenCount = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (inputArray[i] % 2 == 0)
+        {
+            outputArray[evenCount] = inputArray[i]; // в вых.массив кладем четные значения
+            evenCount++;
+        }
+    }
+    return evenCount;
+}
+
+int SizeArray = 2; // чтобы можно было менять размер только в одном месте
+int[] firstArray = new int[SizeArray];
+
+FillArray(firstArray);
+Console.WriteLine("Исходный массив: ");
+PrintArray(firstArray, firstArray.Length);
+
+int[] resultArray = new int[SizeArray];
+int evenCount = SearchEven(firstArray, resultArray);
+
+if (evenCount == 0)
 {
     Console.WriteLine("Массив не содержит четных чисел.");
 }
 else
 {
     Console.WriteLine("Четные числа в массиве: ");
-    for (int i = 0; i < position; i++)
-    {
-        Console.Write($"{ResultArray[i]} ");
-    }
-    Console.WriteLine();
+    PrintArray(resultArray, evenCount);
 }
-
